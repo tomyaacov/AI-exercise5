0c3fc87b66ac6f0c55ecc2884ef4f81aca6d3193
tomyaacov <tomyaacov1210@gmail.com>
2018-10-30 18:27:50 +0200

    greedy agent addition

--- src/main/java/agent/GreedyAgent.java
+++ src/main/java/agent/GreedyAgent.java
@@ -11,23 +11,52 @@ public class GreedyAgent extends Agent {
         super(context);
     }
 
-    //@Override
-    //public HurricaneNode doNextAction(double currTime) {
-    //    double shortestPathDist = Double.MAX_VALUE;
-    //    DijkstraOutput output = Algorithm.dijkstra(getContext(), getCurrNode(), getPeople());
-    //    //if(getPeople() > 0){
-    //    //    for(int = 1; i < outp)
-    //    //}
-    //    return null;
-    //}
-//
-    //@Override
-    //public HurricaneNode traverse() {
-    //    return null;
-    //}
-
     @Override
     public AgentAction doNextAction(double currTime) {
+        double shortestPathDist = Double.MAX_VALUE;
+        Integer targetNode;
+        HurricaneNode checkedNode;
+
+        DijkstraOutput output = Algorithm.dijkstra(context, getCurrNode(), getPeople());
+
+        if (getPeople() > 0){
+            for(int i = 0; i < output.getDist().size(); i++){
+                if (i+1 != Integer.parseInt(getCurrNode().getId())){//checking it is not the same node
+                    checkedNode = context.getGraph().getNode(Integer.toString(i+1));
+                    if (checkedNode.isShelter()){//looking for shelters only
+                        if(shortestPathDist > output.getDist().get(i)){
+                            shortestPathDist = output.getDist().get(i);
+                            targetNode = i;
+                        }
+                    }
+
+                }
+            }
+        } else {
+            for(int i = 0; i < output.getDist().size(); i++){
+                if (i+1 != Integer.parseInt(getCurrNode().getId())){//checking it is not the same node
+                    checkedNode = context.getGraph().getNode(Integer.toString(i+1));
+                    if (checkedNode.getPeople() > 0){//looking for vertex with people only
+                        if(shortestPathDist > output.getDist().get(i)){
+                            shortestPathDist = output.getDist().get(i);
+                            targetNode = i;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        if (shortestPathDist == Double.MAX_VALUE){
+            //return noOPAction(currTime);
+            return null;
+        } else {
+            return null;
+        }
+
+    }
+
+    public AgentAction traverse(double currTime){
         return null;
     }
 }
