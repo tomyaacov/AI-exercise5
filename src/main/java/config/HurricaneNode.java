12ae2f14a3d8d517debf9db330676178873730ef
aviros <avirosman@gmail.com>
2019-01-14 11:58:05 +0200

    flooding

--- src/main/java/config/HurricaneNode.java
+++ src/main/java/config/HurricaneNode.java
@@ -7,14 +7,13 @@ import org.graphstream.graph.implementations.SingleNode;
 
 public class HurricaneNode extends SingleNode {
 
-    @Getter @Setter
-    private double floodingProb;
+
 
     public HurricaneNode(AbstractGraph graph, String id) {
         super(graph, id);
     }
 
     public String toString(){
-        return "node id:" + getId() + " ,flooding: " + getFloodingProb();
+        return "node id:" + getId();
     }
 }
