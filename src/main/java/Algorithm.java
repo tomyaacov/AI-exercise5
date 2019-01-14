52f29eb7c18a839dc40a820f5578e28f7f755a2c
tomyaacov <tomyaacov1210@gmail.com>
2019-01-14 19:21:43 +0200

    state equals

---
+++ src/main/java/Algorithm.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Algorithm {
+
+    public static void ValueIteration(MDP mdp, double discount, double epsilon){
+        double tempValue;
+        double delta = Double.MAX_VALUE;
+        while (delta < epsilon){
+            for (State s: mdp.getStates()){
+                tempValue = s.getReward() + discount*getMaxExcpectedValue(mdp, s);
+                if (Math.abs(tempValue-s.getUtility()) > delta){
+                    delta = Math.abs(tempValue-s.getUtility());
+                }
+                s.setUtility(tempValue);
+            }
+        }
+    }
+
+    public static double getMaxExcpectedValue(MDP mdp, State s){
+        double maxExpectedValue = -Double.MAX_VALUE;
+        Action bestAction = null;
+        List<Action> possibleActions = mdp.getAllActions(s);
+        for(Action a : possibleActions){
+            double currentExpectedValue = 0;
+            List<StateProbability> stateProbabilities = mdp.transitionFunction(s, a);
+            for (StateProbability sp : stateProbabilities){
+                currentExpectedValue += sp.getValue();
+            }
+            if (currentExpectedValue > maxExpectedValue){
+                maxExpectedValue = currentExpectedValue;
+                bestAction = a;
+            }
+        }
+        s.setBestAction(bestAction);
+        return maxExpectedValue;
+    }
+}
